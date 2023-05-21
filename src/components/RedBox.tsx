fbbccf0acbdab620aad582aa7afcb47cf3d3817e
donoh <a@a.com>
2023-05-21 18:28:24 +0900

    feat : RedBox 추가

---
+++ src/components/RedBox.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+function RedBox() {
+    return <Box />
+}
+
+export default RedBox;
+
+const Box = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: red;
+`
